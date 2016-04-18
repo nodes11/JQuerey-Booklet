f7f3412d10c6e689c2cfab820abe59ce07782ed2
nodes11 <desmarks11@gmail.com>
2016-04-18 15:09:32 -0700

    done

--- scripts/booklet.js
+++ scripts/booklet.js
@@ -4,10 +4,10 @@ var currentP;
 //Just add tabnames and everything else will be done for you!
 var tabNames = ["Aperture", "Shutter Speed", "ISO", "Explore"];
 var htmltext = [
-  "hello<br>hello again 1",
-  "hello<br>hello again 2",
-  "hello<br>hello again 3",
-  "hello<br>hello again 4"
+  "<h1>Aperture</h1><p>Aperture is the size of the hole in the lens that light passes through to reach the sensor. Typically measured in F stops, or the focal ratio, as the a F stop grows in value the size of the hole gets smaller, letting in less light. As the F stop decreases, the size of the hole gets bigger, letting in more light.</p>",
+  "<h1>Shutter Speed</h1><p>Shutter speed is the amount of time that light let into the sensor. With a longer shutter speed you will achieve a brighter image. With a shorter shutter speed you will achieve a darker image. Shutter speed is typically measured in whole seconds and fractions of a second.</p><p>Below are 3 different photos of the same person running, each photo is shot at a progressively faster shutter speed.</p><br><img src='images/run_slow.jpg'></img><p>1/15 of a second shutter speed. Notice that the subject is focus but the background is blurred out due to the movement of the camera.</p><br><img src='images/run_medium.jpg'></img><p>1/125 of a second</p><br><img src='images/run_fast.jpg'></img><p>1/800 of a second</p><br>",
+  "<h1>ISO</h1><p>ISO is the measure of how sensitive your camera is to the light. ISO values range anywhere from 50 to 409,600 depending on the camera. At a lower ISO the camera's image sensor, or the part that creates the image, will not pick up certain things in environments with less light. At a higher ISO, the camera's sensor will be able to capture these features that may be hidden in the darkness.</p><p> All of this comes at a price; as the level of ISO rises, the quality of the image, or the amount grain visible in the photo will increase. In the photos below, see if you can notice the change in ISO.</p><br><img src='images/iso_320.jpg'></img><p style='padding-left: 8.5%;'>ISO 320</p><br><img src='images/iso_1000.jpg'></img><p style='padding-left: 8.5%;'>ISO 1000</p><br><img src='images/iso_6400.jpg'></img><p style='padding-left: 8.5%;'>ISO 6400</p><br><img src='images/iso_12800.jpg'></img><p style='padding-left: 8.5%;'>ISO 12800</p><br><img src='images/iso_24600.jpg'></img><p style='padding-left: 8.5%;'>ISO 25600</p><p>Think about the ISO you would use given these situations:<ol><li>Outside on a bright, sunny day</li><li>In a dark alley way with very little incoming light</li><li>In a football stadium at night</li></ol></p>",
+  "<h1>More</h1><p></p>"
 ];
 
 
@@ -47,6 +47,20 @@ document.addEventListener("DOMContentLoaded", function (event) {
 
       for (var i = 0; i < te.childNodes.length + 1; i++){
         if (te.childNodes[i].id == tempEle2){
+
+          if (i == 1){
+            $(".a").toggleClass("clicked");
+          }
+          else if (i == 2){
+            $(".ss").toggleClass("clicked");
+          }
+          else if (i == 3){
+            $(".i").toggleClass("clicked");
+          }
+          else if (i == 4){
+            $(".m").toggleClass("clicked");
+          }
+
           if (currentP){
             currentP.style.display = "none";
           }
@@ -77,7 +91,20 @@ document.addEventListener("DOMContentLoaded", function (event) {
     }
   });
 
-  $("#side").on("click", function(event){
+  $(".back").on("click", function(event){
+    if ($(".a").hasClass("clicked")){
+      $(".a").removeClass("clicked");
+    }
+    if ($(".ss").hasClass("clicked")){
+      $(".ss").removeClass("clicked");
+    }
+    if ($(".i").hasClass("clicked")){
+      $(".i").removeClass("clicked");
+    }
+    if ($(".m").hasClass("clicked")){
+      $(".m").removeClass("clicked");
+    }
+
     if (currentP){
       currentP.style.display = "none";
     }
